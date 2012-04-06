354edab990f91c3319f89f353a2434d5bcedc6c4
Harvest Zhang <hlzhang@dynamic-oit-vapornet-c-2364.princeton.edu>
2012-04-06 17:57:58 -0400

    No major changes, orientation changed to Portrait Only

--- Rendezvous/rdvSecondViewController.m
+++ Rendezvous/rdvSecondViewController.m
@@ -16,14 +16,14 @@
 
 - (void)viewDidLoad
 {
-    [super viewDidLoad];
+   [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
 }
 
 - (void)viewDidUnload
 {
-    [super viewDidUnload];
-    // Release any retained subviews of the main view.
+   [super viewDidUnload];
+   // Release any retained subviews of the main view.
 }
 
 - (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
