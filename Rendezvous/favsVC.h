cbb12a17c615f679c3ca3bfbbcf68a45c3cc5d4d
Harvest Zhang <hlzhang@princeton.edu>
2012-04-10 02:06:15 -0400

    Renamed files, laid out rough tab and window structure, map view now works (shows pin for current location; default iOS behavior). Skeleton GUI now in place.

---
+++ Rendezvous/favsVC.h
@@ -0,0 +1,13 @@
+//
+//  rdvFavsViewController.h
+//  Rendezvous
+//
+//  Created by Harvest Zhang on 4/5/12.
+//  Copyright (c) 2012 Princeton University. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface favsVC : UIViewController
+
+@end
---
+++ Rendezvous/favsVC.m
@@ -0,0 +1,34 @@
+//
+//  rdvFirstViewController.m
+//  Rendezvous
+//
+//  Created by Harvest Zhang on 4/5/12.
+//  Copyright (c) 2012 Princeton University. All rights reserved.
+//
+
+#import "favsVC.h"
+
+@interface favsVC ()
+
+@end
+
+@implementation favsVC
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)viewDidUnload
+{
+    [super viewDidUnload];
+    // Release any retained subviews of the main view.
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+   return (interfaceOrientation != UIInterfaceOrientationPortraitUpsideDown);
+}
+
+@end
