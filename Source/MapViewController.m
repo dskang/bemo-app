eaf7553ad9f85a64d71745db7e1ab1f6659d1874
Dan Kang <dan@dskang.com>
2012-05-09 10:02:48 -0400

    Get rid of old FIXME

--- Source/MapViewController.m
+++ Source/MapViewController.m
@@ -85,7 +85,6 @@
 }
 
 - (void)endConnection {
-    // FIXME: This shows connecting screen instead of contacts screen (when run from signal, not button)
     [self stopUpdating];
     NSLog(@"Segue: Map -> Contacts");
     [self performSegueWithIdentifier:@"endMapView" sender:nil];
