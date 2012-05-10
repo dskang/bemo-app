ae6abcc9f229ecb40d1565725f137cc7f0fddd15
Dan Kang <dan@dskang.com>
2012-05-10 03:22:54 -0400

    Clear notifications when app becomes active

--- Source/LumoAppDelegate.m
+++ Source/LumoAppDelegate.m
@@ -185,11 +185,6 @@
 - (void)applicationWillEnterForeground:(UIApplication *)application
 {
     // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
-    
-    // Clear notifications from Notification Center
-    [[UIApplication sharedApplication] setApplicationIconBadgeNumber: 1];
-    [[UIApplication sharedApplication] setApplicationIconBadgeNumber: 0];
-    [[UIApplication sharedApplication] cancelAllLocalNotifications];
 }
 
 - (void)applicationDidBecomeActive:(UIApplication *)application
@@ -198,6 +193,11 @@
 
     // Extend the Facebook access token
     [self.auth.facebook extendAccessTokenIfNeeded];
+
+    // Clear notifications from Notification Center
+    [[UIApplication sharedApplication] setApplicationIconBadgeNumber: 1];
+    [[UIApplication sharedApplication] setApplicationIconBadgeNumber: 0];
+    [[UIApplication sharedApplication] cancelAllLocalNotifications];
 }
 
 - (void)applicationWillTerminate:(UIApplication *)application
